04084ac923091ed7b100fe1ac2f250f38507a43d
fabianjm-prog <fabianloky614@gmail.com>
2025-04-15 00:08:23 -0600

    Agregar archivos de proyecto.

---
+++ Cliente_ProyectoFinal/Controllers/UsuarioController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cliente_ProyectoFinal.Controllers
+{
+    public class UsuarioController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
