6a1514f38ff6999b7d8a2d21111bb74b1427f18b
fabianjm-prog <fabianloky614@gmail.com>
2025-04-18 18:05:00 -0600

    cambios READ

--- Cliente_ProyectoFinal/Servicios/Class_CreditoServicio.cs
+++ Cliente_ProyectoFinal/Servicios/Class_CreditoServicio.cs
@@ -10,45 +10,45 @@ namespace Cliente_ProyectoFinal.Servicios
     public class class_CreditoServicio
     {
         private readonly string _baseUrl = "https://localhost:7076/api/";  //Create
-        //private readonly string _baseUrl = "https://localhost:7076/api/"; //Update
-        //private readonly string _baseUrl = "https://localhost:7076/api/"; //Delete
-        //private readonly string __baseUrl = "http://localhost:5109/api/"; //Read
+                                                                           //private readonly string _baseUrl = "https://localhost:7076/api/"; //Update
+                                                                           //private readonly string _baseUrl = "https://localhost:7076/api/"; //Delete
+        private readonly string __baseUrl = "https://localhost:7195/api/"; //Read
 
-        //public async Task<List<Class_Credito>> ObtenerCreditoAsync(string token)
-        //{
-        //    using (var client = new HttpClient())
-        //    {
-        //        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-        //        var response = await client.GetAsync(__baseUrl + "Controller_Credito/ListaCredito");
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            var json = await response.Content.ReadAsStringAsync();
-        //            var jobject = JObject.Parse(json);
-        //            var productos = jobject["value"]?.ToObject<List<Class_Credito>>();
-        //            return productos ?? new List<Class_Credito>();
-        //        }
-               
-        //        return new List<Class_Credito>();
-        //    }
-        //}
+        public async Task<List<Class_Credito>> ObtenerCreditoAsync(string token)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await client.GetAsync(__baseUrl + "Credito/ListaCredito");
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var jobject = JObject.Parse(json);
+                    var productos = jobject["value"]?.ToObject<List<Class_Credito>>();
+                    return productos ?? new List<Class_Credito>();
+                }
 
-        //public async Task<List<Class_Credito>> BuscarCreditoAsync(string CedulaP, string token)
-        //{
-        //    using (var client = new HttpClient())
-        //    {
-        //        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-        //        var response = await client.GetAsync(__baseUrl + "Controller_Credito/BuscarCredito/{CedulaP}");
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            var json = await response.Content.ReadAsStringAsync();
-        //            var jobject = JObject.Parse(json);
-        //            var productos = jobject["value"]?.ToObject<List<Class_Credito>>();
-        //            return productos ?? new List<Class_Credito>();
-        //        }
-        //        return new List<Class_Credito>();
-     
-        //    }
-        //} 
+                return new List<Class_Credito>();
+            }
+        }
+
+        public async Task<List<Class_Credito>> BuscarCreditoAsync(string CedulaP, string token)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await client.GetAsync(__baseUrl + "Controller_Credito/BuscarCredito/{CedulaP}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var jobject = JObject.Parse(json);
+                    var productos = jobject["value"]?.ToObject<List<Class_Credito>>();
+                    return productos ?? new List<Class_Credito>();
+                }
+                return new List<Class_Credito>();
+
+            }
+        }
 
         public async Task<string> CrearCreditoAsync(Class_Credito credito, string token)
         {
