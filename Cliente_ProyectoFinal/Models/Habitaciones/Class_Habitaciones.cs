b2f05249e2d7220d4311b323614c27a6c996fa52
fabianjm-prog <fabianloky614@gmail.com>
2025-04-18 18:27:26 -0600

    dasd
    asd

---
+++ Cliente_ProyectoFinal/Models/Habitaciones/Class_Habitaciones.cs
@@ -0,0 +1,13 @@
+﻿namespace Cliente_ProyectoFinal.Models.Habitaciones
+{
+    public class Class_Habitaciones
+    {
+        public int habitacion_ID { get; set; }
+
+        public string numero { get; set; }
+
+        public int tipo_ID { get; set; }
+
+        public string estado { get; set; }
+    }
+}
