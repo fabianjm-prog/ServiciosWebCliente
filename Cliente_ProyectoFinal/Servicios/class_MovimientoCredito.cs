3e54b0b7616e3fd66d922f58a72120610e31fe41
Greivin Badilla <davidbadilla328@gmail.com>
2025-04-18 23:21:13 -0600

    vista usuario

---
+++ Cliente_ProyectoFinal/Servicios/class_MovimientoCredito.cs
@@ -0,0 +1,25 @@
+﻿using Cliente_ProyectoFinal.ApiUrls;
+using Cliente_ProyectoFinal.Models.MovimientoCredito;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Cliente_ProyectoFinal.Servicios
+{
+    public class class_MovimientoCredito
+    {
+        public async Task<bool> ActualizarMovimientoAsync(int id, Class_MovimientoCredito movimiento, string token)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+                var json = JsonConvert.SerializeObject(movimiento);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await client.PutAsync(Class_Url.UpdateUrl + $"MovimientosCredito/ActualizarMovimiento/{id}", content);
+                return response.IsSuccessStatusCode;
+            }
+        }
+    }
+}
